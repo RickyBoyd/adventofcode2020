e79a3cd1aa5601668b62d6c42385389fbf3ed326
RickyBoyd <ricky_boyd@hotmail.co.uk>
2020-12-30 15:23:00 +0000

    day 4 completed

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -92,3 +92,48 @@
     (reduce +
             (for [line (range down (count map) down)]
               (if (space? map (* (/ line down) right) line) 0 1)))))
+
+(defn split-file-by-blanks
+  [filename]
+  (clojure.string/split (slurp filename) #"\n\n"))
+
+(defn valid-passport?
+  [passport]
+  (some?
+   (and
+    (re-find #"byr" passport)
+    (re-find #"iyr" passport)
+    (re-find #"eyr" passport)
+    (re-find #"hgt" passport)
+    (re-find #"hcl" passport)
+    (re-find #"ecl" passport)
+    (re-find #"pid" passport))))
+
+(defn d4-1
+  []
+  (let [passports (split-file-by-blanks "input_d4-1.txt")]
+    (reduce (fn [cnt pport] (if (valid-passport? pport) (inc cnt) cnt)) 0 passports)))
+
+(defn valid-passport2?
+  [passport-in]
+  (let [passport (str passport-in " ")]
+  (boolean
+   (and
+    (re-find #"byr:(19[2-9][0-9]|200[0-2])\s" passport)
+    (re-find #"iyr:(201[0-9]|2020)\s" passport)
+    (re-find #"eyr:(202[0-9]|2030)\s" passport)
+    (let [[_ number-str type] (re-find #"hgt:([0-9]+)(cm|in)\s" passport)]
+      (and number-str type
+           (let [number (Integer/parseInt number-str)]
+             (cond
+               (= type "cm") (<= 150 number 193)
+               (= type "in") (<= 59 number 76)
+               :else nil))))
+    (re-find #"hcl:#([a-f]|[0-9]){6}\s" passport)
+    (re-find #"ecl:(amb|blu|brn|gry|grn|hzl|oth)\s" passport)
+    (re-find #"pid:([0-9]{9})\s" passport)))))
+
+(defn d4-2
+  []
+  (let [passports (split-file-by-blanks "input_d4-2.txt")]
+    (reduce (fn [cnt pport] (if (valid-passport2? pport) (inc cnt) cnt)) 0 passports)))
